c9270d078acff9807286ee408ff82c2e17d2709e
jvillarejo <arzivian87@gmail.com>
2014-11-12 10:52:17 -0300

    added html skeleton

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  
+  def start
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: 'home#start'
+  
   resources :casas, only: [:index, :show, :create]
   resources :fuerzas, only: [:index, :show, :create]
   resources :heroes, only: [:index, :show, :create]
